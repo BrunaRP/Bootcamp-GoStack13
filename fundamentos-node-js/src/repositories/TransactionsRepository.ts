89bd2e74937a19241bc7426dcd250946f338301e
Bruna Polonio <polonio.bruna@gmail.com>
2020-09-28 10:39:11 -0300

    desafio de fundamentos-node-js finalizado e consertando bug no desafio-react-js

---
+++ fundamentos-node-js/src/repositories/TransactionsRepository.ts
@@ -0,0 +1,62 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Response {
+  transactions: Transaction[];
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const balance = this.transactions.reduce(
+      (increment: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          increment.income += transaction.value;
+          increment.total += transaction.value;
+        } else {
+          increment.outcome += transaction.value;
+          increment.total -= transaction.value;
+        }
+
+        return increment;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
---
+++ fundamentos-node-js/src/services/CreateTransactionService.ts
@@ -0,0 +1,37 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if(type !== 'income' && type !== 'outcome') {
+      throw Error('This type is not allowed');
+    }
+
+    if(type === 'outcome' && value > total) {
+      throw Error('You dont have this amount for withdrawal');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
